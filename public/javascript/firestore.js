99d970e059e976c5a5558115a93a8792a4ca3b55
DylanDoyleX00157108 <70896218+DylanDoyleX00157108@users.noreply.github.com>
2020-09-07 13:06:44 +0100

    Add files via upload

---
+++ public/javascript/firestore.js
@@ -0,0 +1,87 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDfSpNv67H7vP_2sPVOtBmAsraOIMX95Lg",
+  authDomain: "y2repeatmessaging.firebaseapp.com",
+  databaseURL: "https://y2repeatmessaging.firebaseio.com",
+  projectId: "y2repeatmessaging",
+  storageBucket: "y2repeatmessaging.appspot.com",
+  messagingSenderId: "481141337804",
+  appId: "1:481141337804:web:980e9b412640c1ab755c69"
+};
+
+// name of the the Firebase collection to be used
+const FB_COLLECTION = "messages";
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+// Get a reference to the database service
+let database = firebase.firestore();
+
+/*
+    Get all messages
+*/
+async function getMessagesAsync() {
+    // Declare empty array
+    let messages = [];
+  
+    // await calls must be made in try-catch blocks
+    try {
+      // Get a snapshot of the products collection
+      let snapshot = await database.collection(FB_COLLECTION).get();
+  
+      // use map() to retrieve product document objects from the snapshot - storing each in the array
+      // map returns each document from the firestore snapshot
+      messages = snapshot.docs.map(doc => {
+        return doc;
+      });
+    } catch (err) {
+      // catch errors
+      console.log(err);
+    }
+  
+    // return the messages array
+    return messages;
+  }
+
+/*
+    Get single message by id from a firebase collection
+*/
+async function getMessageByIdAsync(id) {
+    // Declare empty product
+    let message;
+  
+    // await calls must be made in try-catch blocks
+    try {
+      // Get product document which matches id
+      product = await database.doc(`${FB_COLLECTION}/${id}`).get();
+  
+    } catch (err) {
+      // catch errors
+      console.log(err);
+    }
+  
+    // return the products array
+    return message;
+  }
+
+
+  /*
+    Delete single message by id from a firebase collection
+*/
+async function deleteMessageByIdAsync(id) {
+  
+    // await calls must be made in try-catch blocks
+    try {
+      // Get product document which matches id
+      await database.doc(`${FB_COLLECTION}/${id}`).delete();
+      return true;
+  
+    } catch (err) {
+      // catch errors
+      console.log(err);
+    }
+  
+    return false;
+  }
---
+++ public/messages.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# for this simple example we use a local mosquitto broker
+# the while will stay in the subscribe loop and act on any messages rxed
+#
+#vars
+#
+# Firebase Vars - from app settings
+PROJECT_ID='y2repeatmessaging'
+COLLECTION_ID='messages'
+API_KEY='AIzaSyDfSpNv67H7vP_2sPVOtBmAsraOIMX95Lg'
+TOKEN=''
+# Firestore rules to allow read/write
+# https://firebase.google.com/docs/firestore/security/get-started
+
+# Build the URL to which data will be sent
+# This uses the app settings defined above
+FIRESTORE_URL="https://firestore.googleapis.com/v1/projects/$PROJECT_ID/databases/(default)/documents/$COLLECTION_ID?&key=$API_KEY"
+# check url
+echo $FIRESTORE_URL
+
+# HTTP Header parameters
+# Data will be JSON
+REQ_HEADER='content-type: application/json'
+# Auth Token if required
+AUTH_HEADER="Authorization: Bearer $TOKEN"
+# Saving data to API via POST request 
+REQ_METHOD='POST'
+# mosquitto_sub -v -h localhost -t myapp/# | while read line
+mosquitto_sub -v -h broker.hivemq.com -t messageapp/#z | while read line
+do
+        # first all we do is echo the line (topic + message) to the screen
+        echo "line: $line\n"
+
+        # assume topic has 4 fields in form field1/field2/field3/field4
+        # cut them out of the topic and put into column vars 1-4
+        column1=`echo $line|cut -f2 -d/`
+        echo "col1: $column1"
+        column2=`echo $line|cut -f3 -d/`
+        echo "col2: $column2"
+        column3=`echo $line|cut -f4 -d/`
+        echo "col3: $column3"
+        column4=`echo $line|cut -f5 -d/`
+        echo -e "col4: $column4\n"            
+
+        # now we work on the message
+        # assume message has 5 fields in form field1,field2,field3,field4
+        # cut them out of the msg and put into column vars
+        msg=`echo "$line"|cut -f2- -d ' '`
+        echo -e "msg: $msg\n"
+        sender=`echo "$msg"|cut -f1 -d,`
+        echo "sender: $sender"
+        createdAt=`echo "$msg"|cut -f2 -d,`
+        echo "createdAt: $createdAt"
+        subject=`echo "$msg"|cut -f3 -d,`
+        echo "Subject: $subject"
+        message=`echo "$msg"|cut -f4 -d,`
+        echo "message: $message"
+        #
+        # now we work on the DB, we want to out the columns(1-6) into the DB
+        #
+        # If $createdAt has a value, use it, otherwise generate a timestamp
+        if [[ $createdAt ]]
+        then
+            now=$createdAt
+        else
+            # generate date stamp
+            now=`date`
+        fi
+
+        # Data to be sent in JSON format
+        JSON='{"fields": {"createdAt": {"stringValue": "'"$now"'"}, "sender": {"stringValue": "'"$sender"'"}, "subject": {"stringValue": "'"$subject"'"}, "message": {"stringValue": "'"$message"'"}}}'
+        echo $JSON
+        # https://askubuntu.com/questions/1162945/how-to-send-json-as-variable-with-bash-curl
+
+        # Use CURL to send POST request + data
+        response=$(curl -X $REQ_METHOD  -H "$REQ_HEADER" --data "$JSON" $FIRESTORE_URL)
+
+        # Show response
+        echo $response
+done
